a96f1423131f70c2c31febee8e6ee941fdca31da
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-06 23:29:36 -0700

    create and respond with JSON web token

--- routes/users.js
+++ routes/users.js
@@ -22,13 +22,49 @@ router.post(
       'Please enter a password with 8 or more characters'
     ).isLength({ min: 8 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if user exists
+      let user = await User.findOne({ email });
+
+      if (user) return res.status(400).json({ msg: 'User already exists' });
+
+      user = new User({ name, email, password });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return JSONwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600000 }, // Chnage to 3600 when deployed
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
