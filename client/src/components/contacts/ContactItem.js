2565439b97a06724ec0f4ddf6826100f447a3cc6
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-08 17:19:19 -0700

    add ContactContext and Contacts and ContactItem components

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ContactItem = ({ contact: { email, id, name, phone, type } }) => {
   return (
@@ -6,6 +7,7 @@ const ContactItem = ({ contact: { email, id, name, phone, type } }) => {
       <h3 className='text-primary text-left'>
         {name}{' '}
         <span
+          style={{ float: 'right' }}
           className={
             `badge ` +
             (type === 'professional' ? 'badge-success' : 'badge-primary')
@@ -14,8 +16,28 @@ const ContactItem = ({ contact: { email, id, name, phone, type } }) => {
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>{' '}
       </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'></i> {phone}
+          </li>
+        )}
+      </ul>
+      <p>
+        <button className='btn btn-dark btn-sm'>Edit</button>
+        <button className='btn btn-danger btn-sm'>Delete</button>
+      </p>
     </div>
   );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
 };
 
 export default ContactItem;
